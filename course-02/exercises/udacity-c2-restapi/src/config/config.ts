630b9db43df202821f751df68be954e605cb0f01
SamW92288 <samw92288@gmail.com>
2021-04-18 11:34:37 -0400

    Update config.ts

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,9 +1,9 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "SamWDevDB",
+    "password": "samwdevdb",
+    "database": "SamWDevDB",
+    "host": "samwdevdb.crw9jykgtcvp.us-east-2.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
